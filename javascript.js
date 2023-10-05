f4299c485bc89e49ba230c6c36065cea4ac84def
Khizer Sohail <khizrsohail@gmail.com>
2023-10-05 03:29:50 +0100

    Adding first javascript file
    Testing output, variables and different data types

---
+++ javascript.js
@@ -0,0 +1,16 @@
+console.log(23 + 97)
+console.log(12+15+45+4+12+45)
+console.log((4+6+9)/77)
+
+let a = 10
+console.log(a)
+console.log(9 * a)
+let b = 7 * a
+console.log(b)
+
+let max = 57
+let actual = max - 13
+let percentage = actual/max
+console.log(percentage)
+
+//what is the difference between const, let and var?
